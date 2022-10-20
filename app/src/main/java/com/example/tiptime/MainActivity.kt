c819866c3c57f9c1a48595050a0f4433fff2559b
Paolo Caldera <paolo.caldera@keypartner.com>
2022-10-20 10:56:54 +0200

    Condition the case of empty input

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 import kotlin.math.ceil
@@ -11,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     // Declare the binding variable so that can be used all over the class
     private lateinit var binding: ActivityMainBinding
 
-    var tip = 0.0
+    private var tip = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         A binding object is created to provide a reference to all the views of the layout.
         When view binding is enabled, a class related to a specific layout is created: in this
         case, activity_main.xml creates a class called ActivityMainBinding.
-        Such class allows to inflate the layout by indicating the root element (Costraint Layout)
+        Such class allows to inflate the layout by indicating the root element (Constraint Layout)
          */
 
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -40,7 +41,14 @@ class MainActivity : AppCompatActivity() {
     private fun calculateTip() {
 
         // Get the decimal input value
-        val costOfService = binding.costOfService.text.toString().toDouble()
+        val costOfService = binding.costOfService.text.toString().toDoubleOrNull()
+
+        // If the provided value is null, clear the tip amount and then notify the user
+        if (costOfService == null || costOfService == 0.0) {
+            displayTip(0.0)
+            Toast.makeText(this, "Cost of service value not provided!", Toast.LENGTH_SHORT).show()
+            return
+        }
 
         // Get the tip percentage selected by the user
         val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
@@ -56,8 +64,13 @@ class MainActivity : AppCompatActivity() {
             tip = ceil(tip)
         }
 
-        // Format the currency according to the phone language selected by the user
-        binding.tipAmount.text =
-            getString(R.string.tip_amount, NumberFormat.getCurrencyInstance().format(tip))
+        displayTip(tip)
+    }
+
+
+    private fun displayTip(tipToDisplay: Double) {
+        // Format the tip with a currency, according to the phone language selected by the user
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tipToDisplay)
+        binding.tipAmount.text = getString(R.string.tip_amount, formattedTip)
     }
 }
